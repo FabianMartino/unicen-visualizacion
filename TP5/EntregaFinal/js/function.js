972dae23fb256fc42f4ea0855141060a3669848d
Fabian Martino <fabian.martino.m@gmail.com>
2017-11-08 23:26:05 -0300

    Quinta entrega

--- TP5/EntregaFinal/js/function.js
+++ TP5/EntregaFinal/js/function.js
@@ -1,5 +1,5 @@
 
-
+ document.body.style.background = "url('css/images/background_t.png')";
 /// --------------------------------------------    Authentication
 //var Codebird = require('cd/codebird');
 // or with leading "./", if the codebird.js file is in your main folder:
@@ -9,7 +9,6 @@
 
 
 var imagenes = [];
-
 /// --------------------------------------------    Authentication
 //var Codebird = require("cd/codebird");
 // or with leading "./", if the codebird.js file is in your main folder:
@@ -18,73 +17,95 @@ var cb = new Codebird;
 cb.setConsumerKey("8Vmq8hzhPIPjYc4EAfGQ3vlke", "8h2fSU4vYhLqeJJmpRQRv9JxwtUP6jkmBU4vgitWR6Gjt859Ov");
 cb.setToken("163239019-rJLe5uULgm7ZVDO8yPVstjugvQRSqB6LzEtiakAV", "FnOHS2LkDQfICIBmlwia4oCsfGrSx4ohBQPOmViuToBTU");
 
+//consulta("perro");
+
+function consulta(busqueda){
+  imagenes = [];
  var params = {
-   // q: consulta que se haria
-      q: "NYC"
-
+   //https://developer.twitter.com/en/docs/tweets/search/api-reference/get-search-tweets
+      q: "#"+busqueda, 			// busqueda a realizar
+	  result_type: "mixed", 	// tipo de busqueda mixta
+      count:50 					// maximo de 50 twitters
   };
+  console.log(params.q);
+	cb.__call(
+	"search_tweets",
+	params,
+	function (reply) {
+	 for (var i = 0; i < reply.statuses.length; i++) {
+	  // var pertenece = false;
+	   var twitter = reply.statuses[i];
+	   if(( twitter.extended_entities && twitter.extended_entities.media[0].type == "photo" )){
+		 /* var hasht = twitter.entities.hashtags; for (var j = 0; j < hasht.length; j++) {if ( hasht[i].text == params.q){pertenece = true;}} if (pertenece){ */
+		   var paquete = {
+			   // https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/extended-entities-object
+			   url: twitter.extended_entities.media[0].media_url_https,
+			   //https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/tweet-object
+			   likes: twitter.favorite_count
+		   }
+			imagenes.push(paquete);
+		 }
+	   }
+	  cargarImagenes();// la encargada de mandar las imagenes a la pagina
+	}
 
-cb.__call(
-"search_tweets",
-params,
-function (reply) {
- for (var i = 0; i < reply.statuses.length; i++) {
-  // var pertenece = false;
-   var twitter = reply.statuses[i];
-   if(( twitter.extended_entities && twitter.extended_entities.media[0].type == "photo" )){
-     /*
-     var hasht = twitter.entities.hashtags;
-     for (var j = 0; j < hasht.length; j++) {
-         if ( hasht[i].text == params.q){
-            pertenece = true;
-         }
-     }
-     if (pertenece){ */
-       var paquete = {
-           // https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/extended-entities-object
-           url: twitter.extended_entities.media[0].media_url_https,
-           //https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/tweet-object
-           likes: twitter.favorite_count
-       }
-        imagenes.push(paquete);
-     }
-   }
-  cargarImagenes();// la encargada de mandar las imagenes a la pagina
-}
 );
+}
 
 function cargarImagenes(){
-  for (var i = 0; i < imagenes.length; i++) {
-      // unir esto con los contenedores correspondientes
+  alert( imagenes.length);
+  var img = document.getElementById("galeria");
+  while (img.hasChildNodes()) {
+      img.removeChild(img.firstChild);
   }
+  var divs = document.createElement('div');
+  divs.className = "row";
+  divs.id = 'grill'
+  for (var i = 0; i < imagenes.length; i++) {
+    console.log(imagenes[i]);
+    var divtamano = document.createElement('div');
+    divtamano.className = "col-md-3";
+    var divclase = document.createElement('div');
+    divclase.className = "imagenesGaleria";
+    var images = document.createElement('img');
+    images.src = imagenes[i].url;
+    images.id = i;
+    images.className = "img-responsive";
+    divclase.appendChild(images);
+    divtamano.appendChild(divclase);
+    divs.appendChild(divtamano);
+
+  }
+  document.getElementById('galeria').appendChild(divs);
 }
+
 $('#hash').submit(function(e){
   e.preventDefault();
-  document.getElementById("ejemplo").style.display = "none";
-  document.getElementById("contenido").style.display = "block";
   document.getElementById("grilla").style.backgroundColor = "lightblue";
   var busqueda = document.getElementById("hash").buscar.value;
-  params.q = busqueda;
-  console.log(params.q);
+  //consulta(busqueda);
+  $("body").load("vista1");
 });
 $('#grilla').click(function(e){
   e.preventDefault();
+  cargarpagina("vista1.html");
   document.getElementById("grilla").style.backgroundColor = "lightblue";
-  document.getElementById("galeria").style.backgroundColor = "white";
+  document.getElementById("exposicion").style.backgroundColor = "white";
   document.getElementById("presentacion").style.backgroundColor = "white";
-
 });
-$('#galeria').click(function(e){
+$('#exposicion').click(function(e){
   e.preventDefault();
+  cargarpagina("vista2.html");
   document.getElementById("grilla").style.backgroundColor = "white";
-  document.getElementById("galeria").style.backgroundColor = "lightblue";
+  document.getElementById("exposicion").style.backgroundColor = "lightblue";
   document.getElementById("presentacion").style.backgroundColor = "white";
-
 });
 $('#presentacion').click(function(e){
-  e.preventDefault();
+  cargarpagina("vista3.html");
   document.getElementById("grilla").style.backgroundColor = "white";
-  document.getElementById("galeria").style.backgroundColor = "white";
+  document.getElementById("exposicion").style.backgroundColor = "white";
   document.getElementById("presentacion").style.backgroundColor = "lightblue";
-
 });
+function cargarpagina(pagina){
+  location.href = pagina;
+}
